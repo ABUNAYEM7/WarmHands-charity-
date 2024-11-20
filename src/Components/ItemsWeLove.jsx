8e7ec8345602d9274363fed97e1237e3853df843
NAYEM07 <mohammadabunayem106@.com>
2024-11-20 14:52:58 +0600

    Implement aos at items we love section

--- src/Components/ItemsWeLove.jsx
+++ src/Components/ItemsWeLove.jsx
@@ -1,21 +1,33 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import image1 from "../assets/cloth-1.PNG";
 import image2 from "../assets/cloth-2.PNG";
 import image3 from "../assets/cloth-3.PNG";
 import donateBannerLeft from "../assets/donateBannerLeft.jpg";
 import { FaFaceGrinHearts } from "react-icons/fa6";
+import "aos/dist/aos.css"; // Import AOS styles
+import AOS from "aos"; // Import AOS
 
 const ItemsWeLove = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); // Initialize AOS with animation duration
+  }, []);
+
   return (
     <div>
-      <div className="my-6 flex flex-col-reverse lg:flex-row gap-5 justify-center items-center">
+      <div
+        className="my-6 flex flex-col-reverse lg:flex-row gap-5 justify-center items-center"
+        data-aos="fade-down"
+      >
         {/* left-container */}
-        <div className="w-full lg:w-1/2  bg-[#007aff] rounded-xl animate__animated animate__fadeInLeftBig">
+        <div className="w-full lg:w-1/2 bg-[#007aff] rounded-xl">
           <h3 className="text-3xl font-bold text-center my-3 text-white flex items-center justify-center gap-2">
-            Items We Love 
-            <span> <FaFaceGrinHearts size={25} /></span>
+            Items We Love
+            <span>
+              <FaFaceGrinHearts size={25} />
+            </span>
           </h3>
-          <div className="flex flex-col  text-white">
+          <div className="flex flex-col text-white">
             <div className="flex items-center justify-center gap-5 p-4">
               <div>
                 <img className="rounded-full" src={image1} alt="" />
@@ -57,19 +69,22 @@ const ItemsWeLove = () => {
         {/* right-image */}
         <div className="w-full lg:w-1/2">
           <img
-            className="lg:min-h-[700px] animate__animated animate__lightSpeedInRight"
+            className="lg:min-h-[700px]"
             src={donateBannerLeft}
             alt="image"
           />
         </div>
       </div>
-      <div className="w-full flex justify-center">
-      <Link
-        to={"/DonateCamp/DonateDetails/1"}
-        className="btn bg-[#007aff] text-white hover:text-[#007aff]"
+      <div
+        className="w-full flex justify-center"
+        data-aos="fade-down"
       >
-        Donate Now
-      </Link>
+        <Link
+          to={"/DonateCamp/DonateDetails/1"}
+          className="btn bg-[#007aff] text-white hover:text-[#007aff]"
+        >
+          Donate Now
+        </Link>
       </div>
     </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar fixed top-0  bg-blue-300/50 backdrop-blur-md z-10">
+    <div className="navbar fixed top-0 left-0  bg-blue-300/50 backdrop-blur-md z-10">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
